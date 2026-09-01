f61111578d8a886740a7b60646e3d4386209423a
callemiguel889-cpu <callemiguel889@gmail.com>
2026-09-01 16:08:10 -0500

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>Toktube App</h1>
-      <p>¡Aplicación desplegada correctamente en Vercel!</p>
-    </main>
-  );
-}
