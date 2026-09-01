388259070799ab85f77dc2baf43c1fcc4f5f0203
callemiguel889-cpu <callemiguel889@gmail.com>
2026-09-01 16:55:49 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Toktube</h1>
+      <p>¡La aplicación está en línea y funcionando!</p>
+    </div>
+  );
+}
